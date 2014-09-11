c943b3fe929873f08b3a5de7d9ef9063e69380f0
bsqqueenie <bsqqueenie@gmail.com>
2014-09-11 21:07:51 +0800

    测试代码

---
+++ project1/config.rb
@@ -0,0 +1,24 @@
+require 'compass/import-once/activate'
+require 'sass-globbing'
+require 'singularitygs'
+require 'singularity-extras'
+require 'susy'
+require 'breakpoint'
+require 'animation'
+require 'zen-grids'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+project_type = :stand_alone
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+line_conmmets = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
+
+
